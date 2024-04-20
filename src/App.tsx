d7fae808b9e7fce8ef158a5ce22c6f05287f2fd8
Lucas Hang <lucas.hhang@gmail.com>
2024-04-19 22:06:48 -0300

    feat: load RobotExpressive.glb

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,46 @@
 import { useEffect, useRef } from "react";
 import * as THREE from "three";
+import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
 
 import "./App.css";
+
+function getDimensions(): [width: number, height: number] {
+  return [window.innerWidth * 0.6, window.innerHeight * 0.6];
+}
 
 function App() {
   const containerRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
-    const w = window.innerWidth;
-    const h = window.innerHeight;
+    const [w, h] = getDimensions();
 
     const scene = new THREE.Scene();
+    scene.background = new THREE.Color(0xffffff);
+
     const camera = new THREE.PerspectiveCamera(75, w / h, 0.1, 1000);
     camera.position.z = 5;
+
     const renderer = new THREE.WebGLRenderer();
-    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.setSize(w, h);
+
     containerRef.current?.appendChild(renderer.domElement);
 
-    const geometry = new THREE.BoxGeometry();
-    const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-    const cube = new THREE.Mesh(geometry, material);
-    scene.add(cube);
+    const loader = new GLTFLoader();
+
+    loader.load(
+      "../src/assets/RobotExpressive.glb",
+      (gltf) => {
+        gltf.scene.position.set(0, -2, 0);
+        scene.add(gltf.scene);
+      },
+      undefined,
+      (error) => {
+        console.error(error);
+      }
+    );
 
     const handleResize = () => {
-      const width = window.innerWidth;
-      const height = window.innerHeight;
+      const [width, height] = getDimensions();
 
       camera.aspect = width / height;
       camera.updateProjectionMatrix();
@@ -37,10 +53,8 @@ function App() {
 
     // Add this function inside the useEffect hook
     const renderScene = () => {
-      cube.rotation.x += 0.01;
-      cube.rotation.y += 0.01;
-
       renderer.render(scene, camera);
+
       requestAnimationFrame(renderScene);
     };
     // Call the renderScene function to start the animation loop
@@ -49,12 +63,13 @@ function App() {
     // Add this inside the useEffect hook
     return () => {
       window.removeEventListener("resize", handleResize);
+      renderer.domElement.remove();
     };
   }, []);
 
   return (
     <main className="container">
-      <h1>Full Stack Generation - Desafio técnico</h1>
+      <h2>Full Stack Generation - Desafio técnico</h2>
       <div ref={containerRef}></div>
     </main>
   );
